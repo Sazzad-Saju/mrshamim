eb26efb5e8b602785030e19fedc0f62608420459
Sazzad-Saju <saju.cse.hstu@gmail.com>
2026-04-16 12:26:43 +0600

    Add: Gallery

--- js/scripts.js
+++ js/scripts.js
@@ -90,6 +90,23 @@ $(function () {
         smartSpeed: 500
     });
 
+    // Favorite Picks slider
+    $('.favorite-slider.owl-carousel').owlCarousel({
+        items: 3,
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        autoplayHoverPause: true,
+        smartSpeed: 800,
+        rtl: true,
+        dots: true,
+        responsive: {
+            0: { items: 1 },
+            768: { items: 2 },
+            992: { items: 3 }
+        }
+    });
 
 
     // magnificPopup
